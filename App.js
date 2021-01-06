d3ef0d450ab5906757f9628ac8d13697da886fde
TheR20 <r20_banana@hotmail.com>
2021-01-06 12:44:54 -0600

    agregado navegacion y logica inicial

--- App.js
+++ App.js
@@ -1,90 +1,142 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import * as React from 'react';
+import {  View,ImageBackground ,StyleSheet,FlatList,TouchableOpacity,Image} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import gifmoviendose from './src/imagen';
+import registros from './src/registros.json';
+import {   Button, Text  } from 'galio-framework'
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  Image, Dimensions,
-} from 'react-native';
-import ImageZoom from 'react-native-image-pan-zoom';
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+function HomeScreen({ navigation }) {
   return (
-    <View>
-      <Text style={styles.topBox2}>strings.textodimemasaoru</Text>
-    <ImageZoom cropWidth={Dimensions.get('window').width}
-                       cropHeight={Dimensions.get('window').height}
-                       imageWidth={200}
-                       imageHeight={200}>
-                <Image style={{width:200, height:200}}
-                       source={{uri:'https://www.poomsaetaekwondo.com/wp-content/uploads/2019/11/ESQUEMA-DEL-POMMSAE-TAEGUK-I-CHANG.jpg'}}/>
-            </ImageZoom>
-            </View>
+
+
+
+
+
+         <FlatList
+                      data={registros}
+                      showsVerticalScrollIndicator={false}
+                      renderItem={({item}) =>
+
+                      {
+
+
+                          return(
+                            <TouchableOpacity
+                             onPress={() => navigation.navigate('Imagen', { IDs: item.IDs,
+                               Nombre: item.Nombre , UrlCom: item.UrlCom, UrlFoto: item.UrlFoto })}>
+                            <View style={styles.artistBox}>
+                           <Image style={styles.image} source={{uri: item.UrlFoto }}/>
+
+                              <View style={styles.info}>
+                             <Text style={styles.titulo}>{item.Nombre}</Text>
+
+                              </View>
+                            </View>
+               </TouchableOpacity>
+
+                            )
+
+
+
+
+
+                      }
+                       }
+                    keyExtractor={(item, index) => index.toString()}
+                    />
+
+
+
+
   );
-};
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Imagen" component={gifmoviendose} options={{headerShown: false}}/>
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-  topBox2: {
-      marginTop: 40,
-      textAlign: 'center', // <-- the magic
-        fontSize: 18,
-        color: "white",
-      backgroundColor: 'rgba(52, 52, 52, 0.8)',
-      fontFamily: 'Iowan Old Style'
-    }
+  artistBox:{
+      margin: 5,
+      backgroundColor: '#7E7F7A',
+      flexDirection: 'row',
+      shadowColor: 'black',
+      shadowOpacity: .2,
+      shadowOffset:{
+        height: 1,
+        width: -2,
+      },
+      elevation: 2,
+    },
+
+    image:{
+      width: 150,
+      height: 150,
+    },
+
+    info:{
+      flex: 1,
+      flexDirection: 'column',
+      alignItems:'center',
+      justifyContent:'center',
+    },
+
+    titulo:{
+      fontWeight: "bold",
+       textAlign: 'center',
+         fontSize: 18,
+       color:'white',
+
+    },
+
+    row:{
+      flexDirection:'row',
+      justifyContent:'space-between',
+      marginHorizontal: 40,
+      marginTop:15,
+    },
+
+    iconContainer:{
+      flex:1,
+      alignItems:'center',
+    },
+    backgroundImage: {
+      paddingTop: 20,
+  flex: 1,
+      resizeMode: 'cover',
+    },
+
 });
+
 
 export default App;
---
+++ src/imagen.js
@@ -0,0 +1,134 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+ImageBackground,
+  View,
+ScrollView,
+  StatusBar,
+  Image, Dimensions,  Linking,TouchableHighlight
+} from 'react-native';
+import ImageZoom from 'react-native-image-pan-zoom';
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import LocalizedStrings from 'react-native-localization';
+import {   Button, Text  } from 'galio-framework'
+let strings = new LocalizedStrings({
+  en:{
+    a1:"Video Example",
+
+  },
+  es:{
+
+    a1:"Video Ejemplo",
+
+  },
+
+});
+
+
+const App: () => React$Node = ({ route, navigation }) => {
+  const image = "https://titanchannel.com/blog/wp-content/uploads/2017/08/Captura-de-pantalla-2017-08-04-a-las-15.15.27.png";
+
+
+  const { UrlCom } = route.params;
+  const { Nombre } = route.params;
+  const { UrlFoto } = route.params;
+
+  return (
+<ImageBackground style={styles.backgroundImage} source={{uri:  image}}>
+    <ScrollView>
+    <View>
+      <Text style={styles.topBox2}> Poomsae: { Nombre }</Text>
+<View style={styles.loginButtonSection}>
+      <Button round uppercase color="#8E9C20" size="large" style={{ marginTop:  20,
+           fontFamily: 'serif', }} onPress={() => {
+    //on clicking we are going to open the URL using Linking
+    Linking.openURL( UrlCom );
+
+  }} >{strings.a1}</Button>
+</View>
+    <ImageZoom cropWidth={Dimensions.get('window').width}
+                       cropHeight={Dimensions.get('window').height}
+                       imageWidth={400}
+                       imageHeight={400}>
+                <Image style={{width:400, height:400}}
+                       source={{uri: UrlFoto }}/>
+            </ImageZoom>
+            </View>
+            </ScrollView>
+              </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+  topBox2: {
+      marginTop: 20,
+      textAlign: 'center', // <-- the magic
+        fontSize: 18,
+        color: "white",
+      backgroundColor: 'rgba(52, 52, 52, 0.8)',
+      fontFamily: 'Iowan Old Style'
+    },
+    loginButtonSection: {
+
+     justifyContent: 'center',
+     alignItems: 'center'
+  },
+  backgroundImage: {
+
+  flex: 1,
+  resizeMode: 'cover',
+},
+});
+
+export default App;
